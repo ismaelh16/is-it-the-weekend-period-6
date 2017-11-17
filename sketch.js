ae352cd0d57b136afd1d8f1015f40dc085e7144d
Ismael Hidalgo <ismaelh50336@students.laalliance.org>
2017-11-17 20:27:26 +0000

    I'm done with my lab.

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var answer = 'NO'
+
+function setup(){
+    createCanvas(windowWidth, windowHeight)
+}
+
+function draw(){
+    background('royalblue')
+    textSize(32)
+    fill('white')
+    text('Is it the weekend? ' + answer, 40, 40)
+}
+
+
+
+
+
+
+
+
